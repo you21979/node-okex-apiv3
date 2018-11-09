4639a8a32a3c9bed359df6bd0b2c7a4836b6427d
you21979 <you2197901@gmail.com>
2018-11-09 13:39:48 +0900

    add example

---
+++ example/instruments.js
@@ -0,0 +1,8 @@
+const okexv3 = require("..")
+const public = okexv3.publicApi
+const cr = okexv3.customRequest
+
+const req = new cr.BaseRequest()
+const papi = new public.FuturesAPI(req)
+papi.instruments().then(console.log)
+
---
+++ example/ticker.js
@@ -0,0 +1,9 @@
+const okexv3 = require("..")
+const public = okexv3.publicApi
+const cr = okexv3.customRequest
+
+const req = new cr.BaseRequest()
+const papi = new public.FuturesAPI(req)
+const inst_id = "BTC-USD-181228"
+papi.ticker(inst_id).then(console.log)
+
