6daaba46fbe835250a3d0728ea6882dedfa9bdb5
you21979 <you2197901@gmail.com>
2018-11-09 13:47:23 +0900

    add

--- example/public/instruments.js
+++ example/public/instruments.js
@@ -1,9 +1,8 @@
-const okexv3 = require("..")
+const okexv3 = require("../..")
 const public = okexv3.publicApi
 const cr = okexv3.customRequest
 
 const req = new cr.BaseRequest()
 const papi = new public.FuturesAPI(req)
-const inst_id = "BTC-USD-181228"
-papi.ticker(inst_id).then(console.log)
+papi.instruments().then(console.log)
 
--- example/public/ticker.js
+++ example/public/ticker.js
@@ -1,8 +1,9 @@
-const okexv3 = require("..")
+const okexv3 = require("../..")
 const public = okexv3.publicApi
 const cr = okexv3.customRequest
 
 const req = new cr.BaseRequest()
 const papi = new public.FuturesAPI(req)
-papi.instruments().then(console.log)
+const inst_id = "BTC-USD-181228"
+papi.ticker(inst_id).then(console.log)
 
