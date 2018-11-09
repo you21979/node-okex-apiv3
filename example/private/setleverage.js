a65255cc357205eef73a26bb1e51e8cc9971b644
you21979 <you2197901@gmail.com>
2018-11-09 17:04:32 +0900

    update

---
+++ example/private/setleverage.js
@@ -0,0 +1,11 @@
+const okexv3 = require("../..")
+const papi = okexv3.privateApi
+const cr = okexv3.customRequest
+
+const fs = require("fs")
+const auth = JSON.parse(fs.readFileSync("./config/auth.json"))
+
+const req = new cr.SignRequest(auth["API-KEY"], auth["API-SECRET"], auth["API-PASS"])
+const wapi = new papi.FuturesAPI(req)
+wapi.postLeverage("BTC", 10).then(console.log)
+
